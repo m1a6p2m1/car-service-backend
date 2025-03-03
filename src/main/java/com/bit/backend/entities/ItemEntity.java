494cacfb5533196ee8e3a7c6e5830d9c47896c9c
m1a6p2m1 <poornamedhavi@gmail.com>
2025-03-04 02:19:00 +0530

    create item image upload delete edit part

--- src/main/java/com/bit/backend/entities/ItemEntity.java
+++ src/main/java/com/bit/backend/entities/ItemEntity.java
@@ -21,11 +21,18 @@ public class ItemEntity {
     private String supplierName;
     @Column(name = "description")
     private String description;
+    @Column(name = "image")
+    private byte[] image;
+    @Column(name = "image_name")
+    private String imageName;
+    @Column(name = "image_type")
+    private String imageType;
 
     public ItemEntity() {
     }
 
-    public ItemEntity(long itemId, String itemCode, String itemName, String itemCategory, String brandName, String supplierName, String description) {
+    public ItemEntity(long itemId, String itemCode, String itemName, String itemCategory, String brandName, String supplierName, String description,
+                        byte[] image, String imageType, String imageName) {
         this.itemId = itemId;
         this.itemCode = itemCode;
         this.itemName = itemName;
@@ -33,6 +40,9 @@ public class ItemEntity {
         this.brandName = brandName;
         this.supplierName = supplierName;
         this.description = description;
+        this.image = image;
+        this.imageName = imageName;
+        this.imageType = imageType;
     }
 
     public long getItemId() {
@@ -89,5 +99,29 @@ public class ItemEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
     }
 }
